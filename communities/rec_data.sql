16b55237229c3980258f0256cd6933c7c949bf56
Stavroula Eleftherakis <selefth@yahoo.com>
2023-10-10 20:09:35 +0300

    > update sql file

--- communities/rec_data.sql
+++ communities/rec_data.sql
@@ -1,13 +1,12 @@
 
 -- Create the interactions table
 CREATE TABLE interactions (
-    author_id INTEGER NOT NULL,          -- Author identifier
-    orcid VARCHAR(255),                  -- ORCID identifier of the author (can be NULL)
+    orcid VARCHAR(255) NOT NULL,         -- ORCID identifier of the author
     result_id INTEGER NOT NULL,          -- Identifier of the cited/result item
     community_id INTEGER NOT NULL,       -- ID of the community
     interaction_type ENUM('authorship', 'cited') NOT NULL, -- Type of interaction
-    PRIMARY KEY (author_id, result_id, community_id),
-    FOREIGN KEY (author_id) REFERENCES authors(id),
+    PRIMARY KEY (orcid, result_id, community_id),
+    FOREIGN KEY (orcid) REFERENCES authors(orcid),
     FOREIGN KEY (result_id) REFERENCES results(id),
     FOREIGN KEY (community_id) REFERENCES communities(id)
 );
@@ -19,31 +18,29 @@ DECLARE
 BEGIN
     FOR community IN (SELECT DISTINCT id FROM communities)
     LOOP
-        INSERT INTO interactions (author_id, orcid, result_id, community_id, interaction_type)
+        INSERT INTO interactions (orcid, result_id, community_id, interaction_type)
         WITH author_written AS (
-            SELECT a.id as author_id, a.orcid, r.id, c.id as community_id, 'authorship' as interaction_type
-            FROM author a
+            SELECT a.orcid, r.id, c.id as community_id, 'authorship' as interaction_type
+            FROM authors a
             JOIN result_author ra ON a.id = ra.author_id
             JOIN result r ON ra.result_id = r.id
             JOIN result_community rc ON r.id = rc.result_id
             JOIN community c ON rc.community_id = c.id
-            WHERE c.id = community
-            -- WHERE a.orcid IS NOT NULL AND a.orcid != '' AND c.id = community
+            WHERE a.orcid IS NOT NULL AND a.orcid != '' AND c.id = community
         ),
         author_cited AS (
-            SELECT a.id as author_id, a.orcid, rcit.result_id_cited, c.id as community_id, 'cited' as interaction_type
-            FROM author a
+            SELECT a.orcid, rcit.result_id_cited, c.id as community_id, 'cited' as interaction_type
+            FROM authors a
             JOIN result_author ra ON a.id = ra.author_id
             JOIN result_citations rcit ON ra.result_id = rcit.result_id_cites
             JOIN result r ON rcit.result_id_cited = r.id
             JOIN result_community rc ON r.id = rc.result_id
             JOIN community c ON rc.community_id = c.id
-            WHERE c.id = community
-            -- WHERE a.orcid IS NOT NULL AND a.orcid != '' AND c.id = community
+            WHERE a.orcid IS NOT NULL AND a.orcid != '' AND c.id = community
         )
-        SELECT author_id, orcid, id, community_id, interaction_type FROM author_written
+        SELECT orcid, result_id, community_id, interaction_type FROM author_written
         UNION ALL
-        SELECT author_id, orcid, id, community_id, interaction_type FROM author_cited;
+        SELECT orcid, result_id, community_id, interaction_type FROM author_cited;
     END LOOP;
 END $$;
 
@@ -52,9 +49,9 @@ CREATE INDEX idx_interactions_community_id ON interactions(community_id);
 
 -- Create materialized views for counts
 CREATE MATERIALIZED VIEW author_interactions_count AS
-SELECT author_id, orcid, COUNT(*) AS interaction_count
+SELECT orcid, COUNT(*) AS interaction_count
 FROM interactions
-GROUP BY author_id, orcid;
+GROUP BY orcid;
 
 CREATE MATERIALIZED VIEW result_interactions_count AS
 SELECT result_id, COUNT(*) AS interaction_count
@@ -68,7 +65,7 @@ CREATE TABLE users_mappings (
     community_id INTEGER,         -- Community ID
     UNIQUE(raw_id, community_id),
     FOREIGN KEY (raw_id) REFERENCES authors(orcid),
-    FOREIGN KEY (community_id) REFERENCES communities(community_id)
+    FOREIGN KEY (community_id) REFERENCES communities(id)
 );
 
 CREATE TABLE items_mappings (
@@ -76,8 +73,8 @@ CREATE TABLE items_mappings (
     raw_id INTEGER,               -- Result id
     community_id INTEGER,         -- Community ID
     UNIQUE(raw_id, community_id),
-    FOREIGN KEY (raw_id) REFERENCES results(result_id),
-    FOREIGN KEY (community_id) REFERENCES communities(community_id)
+    FOREIGN KEY (raw_id) REFERENCES results(id),
+    FOREIGN KEY (community_id) REFERENCES communities(id)
 );
 
 -- Create an index on community id and raw id for faster queries
